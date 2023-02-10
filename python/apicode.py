eef334fee2884ca319407f407f0440979e4456e5
Najiy Wahab <najiy.wa@googlemail.com>
2023-02-10 17:01:20 +0000

    changes to apicode

--- python/apicode.py
+++ python/apicode.py
@@ -29,39 +29,11 @@ from copy import deepcopy
 import pprint as pp
 import math
 
+######################################
 
-# print('50 x 84', 50 * math.tan(84/2 * math.pi / 180))
-# print('80 x 65.5', 80 * math.tan(65.5/2 * math.pi / 180))
-# print('80 x 45.5', 80 * math.tan(55/2 * math.pi / 180))
-# input()
-
-
-# camera_settings = {
-#     'hasselblad': 84,
-#     'l1d-20c': 65.5
-# }
-
-# tag_name = 'A1'
 model_file = f"Models/AcaciaNorthBearing_0.86_1674070689.h5"
-image_folder = f"Images"
+# image_folder = f"Images"
 
-# south_bearing = ['DJI_0098', 'DJI_0110',
-#                  'DJI_0241', 'DJI_244', 'DJI_142', 'DJI_249', 'DJI_262', 'DJI_381', 'DJI_405', 'DJI_401', 'DJI_484']
-
-
-south_bearing = {
-    "A1": [(96, 99), (109, 122), (137, 150), (160, 163)],
-    "A2": [(232, 232), (240, 252), (269, 280), (288, 288)],
-    "A3": [(358, 359), (368, 381), (398, 408), (415, 415)],
-    "A4": [(475, 476), (484, 495), (510, 520), (529, 529)],
-    "A5": [(578, 580), (589, 600), (614, 624), (632, 634)],
-    "A6": [(687, 688), (696, 707), (723, 737), (750, 756)],
-    "A7": [(809, 812), (823, 835), (850, 863), (873, 876)],
-}
-
-
-# print( sum([[x for x in range(y[0], y[1]+1)] for y in south_bearing['A2']], []))
-# input()
 
 
 camera_settings = {
@@ -74,28 +46,12 @@ camera_settings = {
     "A7": {"fov": 65.5, "ralt": 80},
 }
 
-# polfile = open(f'Sorted/Other/DJI_0213.JPG.json','r')
-# jpolys = json.load(polfile)
-
-# 5472 x 3648 px (3:2)
-
-# FOv = 84°
-# （X）HFOV = 69.9°
-# （Y）VFOV = 46.6°
-
-
-# fov = 84
-# cam_height = 50
-
-
-img_width = 5472
-img_height = 3648
+# img_width = 5472
+# img_height = 3648
 
 width_ratio = 3
 length_ratio = 2
 hypotenuse = (width_ratio**2 + length_ratio**2) ** 0.5
-
-# semantic_segment_trim = 4  # 0-4
 
 
 #########################################
@@ -110,95 +66,60 @@ feature = {
 }
 
 
-def decimal_coords(coords, ref):
-    decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
-    if ref == "S" or ref == "W":
-        decimal_degrees = -decimal_degrees
-    return decimal_degrees
-
-
-def image_coordinates(image_path):
-    with open(image_path, "rb") as src:
-        img = EXIMAGE(src)
-    if img.has_exif:
-        try:
-            # img.gps_longitude
-            # for i in img:
-            #     pp.pprint(img[i])
-            # pp.pprint(dir(img))
-            # input("done")
-
-            coords = (
-                decimal_coords(img.gps_latitude, img.gps_latitude_ref),
-                decimal_coords(img.gps_longitude, img.gps_longitude_ref),
-            )
-        except AttributeError:
-            print("No Coordinates")
-    else:
-        print("The Image has no EXIF information")
-    print(f"Image {src.name}, OS Version:{img.get('software', 'Not Known')} ------")
-    # print(f"Was taken: {img.datetime_original}, and has coordinates:{coords}")
-
-    return (coords[0], coords[1], img.datetime_original)
 
 
 #########################################
 
 
-# folders = os.listdir(image_folder)
-# input(folders)
 
-# south_bearing = images
+def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG", rotate_180=False) -> object:
 
-# print(images)
-# input()
+    def image_coordinates(image_path):
 
+        def decimal_coords(coords, ref):
+            decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
+            if ref == "S" or ref == "W":
+                decimal_degrees = -decimal_degrees
+            return decimal_degrees
 
-def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> object:
+        with open(image_path, "rb") as src:
+            img = EXIMAGE(src)
+        if img.has_exif:
+            try:
+                coords = (
+                    decimal_coords(img.gps_latitude, img.gps_latitude_ref),
+                    decimal_coords(img.gps_longitude, img.gps_longitude_ref),
+                )
+            except AttributeError:
+                print("No Coordinates")
+        else:
+            print("The Image has no EXIF information")
+        print(
+            f"Image {src.name}, OS Version:{img.get('software', 'Not Known')} ------")
+
+        return (coords[0], coords[1], img.datetime_original)
+    
 
     model = tf.keras.models.load_model(model_file)
     model.summary()
     probability_model = keras.Sequential([model, tf.keras.layers.Softmax()])
 
-    rotate_180 = False
-
     ratio_coeff = camera_settings["A1"]["fov"] / hypotenuse
     hfov = width_ratio * ratio_coeff
     lfov = length_ratio * ratio_coeff
 
-    h_distance = camera_settings["A1"]["ralt"] * math.tan(hfov / 2 * math.pi / 180) * 2
-    l_distance = camera_settings["A1"]["ralt"] * math.tan(lfov / 2 * math.pi / 180) * 2
-
-    h_metre_per_pix = h_distance / img_width
-    l_metre_per_pix = l_distance / img_height
-
-    # print(h_metre_per_pix * 96)
-    # input()
-
-    lat_per_pix = 0.000001 * h_metre_per_pix / 0.11
-    lon_per_pix = 0.000001 * l_metre_per_pix / 0.11
-
-    # if folder not in south_bearing.keys():
-    #     continue
-
-    # images = f'{image_folder}/{folder}/Autonomous Flight'
-    coverage_sem4 = []
-    coverage_sem3 = []
-    coverage_sem2 = []
-    coverage_sem1 = []
-    image_boundaries = []
-    stats = {}
-
-    # imgNo = int(imagefile.replace(
-    #     "_", " ").replace(".", " ").split(" ")[-2])
-
-    # if '.jpg' not in imagefile.lower():
-    #     continue
+    h_distance = camera_settings["A1"]["ralt"] * \
+        math.tan(hfov / 2 * math.pi / 180) * 2
+    l_distance = camera_settings["A1"]["ralt"] * \
+        math.tan(lfov / 2 * math.pi / 180) * 2
 
     coords = image_coordinates(imagefile)
-
-    # print(gps_info)
     img = Image.open(imagefile)
+
+    h_metre_per_pix = h_distance / img.width
+    l_metre_per_pix = l_distance / img.height
+    lat_per_pix = 0.000001 * h_metre_per_pix / 0.11
+    lon_per_pix = 0.000001 * l_metre_per_pix / 0.11
 
     if rotate_180:
         img = img.rotate(180)
@@ -214,10 +135,19 @@ def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> obje
     maxx = len(data[0])
     imgArr = []
 
+
+    coverage_sem4 = []
+    coverage_sem3 = []
+    coverage_sem2 = []
+    coverage_sem1 = []
+    image_boundaries = []
+    stats = {}
+
+
     for y in range(0, math.floor(maxy / 96) * 96, 96):
         for x in range(0, math.floor(maxx / 96) * 96, 96):
             try:
-                d = deepcopy(data[y : y + 96, x : x + 96])
+                d = deepcopy(data[y: y + 96, x: x + 96])
                 imgArr.append(d)
             except Exception as e:
                 print(e)
@@ -298,74 +228,81 @@ def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> obje
                     #                 occupied_bounds.count(False)])
                     # print(occupied_bounds.count(False))
 
-                    if occupied_bounds.count(True) < 4:
-                        # covergae_filtered.append([int(x+96/2), int(y+96/2)])
-                        pass
-                    else:
+                    # if occupied_bounds.count(True) < 4:
+                    #     # covergae_filtered.append([int(x+96/2), int(y+96/2)])
+                    #     pass
+                    # else:
 
-                        # diag tl-br
-                        for pi in range(0, 90):
-                            # for xi in range(0, 95):
-                            datacpy1[y + pi, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[y + pi, x + pi + 1] = [255.0, 0.0, 0.0]
-                            datacpy1[y + pi, x + pi + 2] = [255.0, 0.0, 0.0]
-                            datacpy1[y + pi, x + pi + 3] = [255.0, 0.0, 0.0]
-                            datacpy1[y + pi, x + pi + 4] = [255.0, 0.0, 0.0]
-                            datacpy1[y + pi, x + pi + 5] = [255.0, 0.0, 0.0]
+                    #     # diag tl-br
+                    #     for pi in range(0, 90):
+                    #         # for xi in range(0, 95):
+                    #         datacpy1[y + pi, x + pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[y + pi, x + pi + 1] = [255.0, 0.0, 0.0]
+                    #         datacpy1[y + pi, x + pi + 2] = [255.0, 0.0, 0.0]
+                    #         datacpy1[y + pi, x + pi + 3] = [255.0, 0.0, 0.0]
+                    #         datacpy1[y + pi, x + pi + 4] = [255.0, 0.0, 0.0]
+                    #         datacpy1[y + pi, x + pi + 5] = [255.0, 0.0, 0.0]
 
-                        # diag bl-tr
-                        for pi in range(0, 90):
-                            # for xi in range(0, 95):
-                            datacpy1[96 + y - pi, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[96 + y - pi + 1, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[96 + y - pi + 2, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[96 + y - pi + 3, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[96 + y - pi + 4, x + pi] = [255.0, 0.0, 0.0]
-                            datacpy1[96 + y - pi + 5, x + pi] = [255.0, 0.0, 0.0]
+                    #     # diag bl-tr
+                    #     for pi in range(0, 90):
+                    #         # for xi in range(0, 95):
+                    #         datacpy1[96 + y - pi, x + pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[96 + y - pi + 1, x +
+                    #                  pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[96 + y - pi + 2, x +
+                    #                  pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[96 + y - pi + 3, x +
+                    #                  pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[96 + y - pi + 4, x +
+                    #                  pi] = [255.0, 0.0, 0.0]
+                    #         datacpy1[96 + y - pi + 5, x +
+                    #                  pi] = [255.0, 0.0, 0.0]
 
-                        # top
-                        try:
-                            if occupied_bounds[0]:
-                                for yi in range(y, y + linewidth):
-                                    for xi in range(x, x + 95):
-                                        datacpy1[yi, xi] = [255.0, 0.0, 0.0]
-                        except:
-                            pass
+                    #     # top
+                    #     try:
+                    #         if occupied_bounds[0]:
+                    #             for yi in range(y, y + linewidth):
+                    #                 for xi in range(x, x + 95):
+                    #                     datacpy1[yi, xi] = [255.0, 0.0, 0.0]
+                    #     except:
+                    #         pass
 
-                        # bottom
-                        try:
-                            if occupied_bounds[1]:
-                                for yi in range(y + 96 - linewidth, y + 96):
-                                    for xi in range(x, x + 95):
-                                        datacpy1[yi, xi] = [255.0, 0.0, 0.0]
-                        except:
-                            pass
+                    #     # bottom
+                    #     try:
+                    #         if occupied_bounds[1]:
+                    #             for yi in range(y + 96 - linewidth, y + 96):
+                    #                 for xi in range(x, x + 95):
+                    #                     datacpy1[yi, xi] = [255.0, 0.0, 0.0]
+                    #     except:
+                    #         pass
 
-                        # left
-                        try:
-                            if occupied_bounds[2]:
-                                for xi in range(x, x + linewidth):
-                                    for yi in range(y, y + 96):
-                                        datacpy1[yi, xi] = [255.0, 0.0, 0.0]
-                        except:
-                            pass
+                    #     # left
+                    #     try:
+                    #         if occupied_bounds[2]:
+                    #             for xi in range(x, x + linewidth):
+                    #                 for yi in range(y, y + 96):
+                    #                     datacpy1[yi, xi] = [255.0, 0.0, 0.0]
+                    #     except:
+                    #         pass
 
-                        # right
-                        try:
-                            if occupied_bounds[3]:
-                                for xi in range(x + 96 - linewidth, x + 96):
-                                    for yi in range(y, y + 96):
-                                        datacpy1[yi, xi] = [255.0, 0.0, 0.0]
-                        except:
-                            pass
+                    #     # right
+                    #     try:
+                    #         if occupied_bounds[3]:
+                    #             for xi in range(x + 96 - linewidth, x + 96):
+                    #                 for yi in range(y, y + 96):
+                    #                     datacpy1[yi, xi] = [255.0, 0.0, 0.0]
+                    #     except:
+                    #         pass
 
                     if occupied_bounds.count(True) == 1:
                         coverage_sem1.append(
                             [
                                 imagefile,
                                 1,
-                                coords[0] - (-0.5 * maxy + y + 96 / 2) * lon_per_pix,
-                                coords[1] - (0.5 * maxx - x + 96 / 2) * lat_per_pix,
+                                coords[0] - (-0.5 * maxy + y +
+                                             96 / 2) * lon_per_pix,
+                                coords[1] - (0.5 * maxx - x +
+                                             96 / 2) * lat_per_pix,
                                 "",
                             ]
                         )
@@ -374,8 +311,10 @@ def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> obje
                             [
                                 imagefile,
                                 2,
-                                coords[0] - (-0.5 * maxy + y + 96 / 2) * lon_per_pix,
-                                coords[1] - (0.5 * maxx - x + 96 / 2) * lat_per_pix,
+                                coords[0] - (-0.5 * maxy + y +
+                                             96 / 2) * lon_per_pix,
+                                coords[1] - (0.5 * maxx - x +
+                                             96 / 2) * lat_per_pix,
                                 "",
                             ]
                         )
@@ -384,8 +323,10 @@ def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> obje
                             [
                                 imagefile,
                                 3,
-                                coords[0] - (-0.5 * maxy + y + 96 / 2) * lon_per_pix,
-                                coords[1] - (0.5 * maxx - x + 96 / 2) * lat_per_pix,
+                                coords[0] - (-0.5 * maxy + y +
+                                             96 / 2) * lon_per_pix,
+                                coords[1] - (0.5 * maxx - x +
+                                             96 / 2) * lat_per_pix,
                                 "",
                             ]
                         )
@@ -394,8 +335,10 @@ def detect_images(imagefile=f"Images/A1/Autonomous Flight/DJI_0096.JPG") -> obje
                             [
                                 imagefile,
                                 4,
-                                coords[0] - (-0.5 * maxy + y + 96 / 2) * lon_per_pix,
-                                coords[1] - (0.5 * maxx - x + 96 / 2) * lat_per_pix,
+                                coords[0] - (-0.5 * maxy + y +
+                                             96 / 2) * lon_per_pix,
+                                coords[1] - (0.5 * maxx - x +
+                                             96 / 2) * lat_per_pix,
                                 "",
                             ]
                         )

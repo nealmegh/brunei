9a2a65f268c1f311c4d77efa0867a9bbdff87d22
Md Abrar Jahin Almazi Bipon <bipon.abrar@gmail.com>
2023-07-23 03:35:31 +0100

    Delete Function Added

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -112,25 +112,25 @@ def download_xls():
 #     return render_template('home/uploadImage.html', form=form, segment='index')
 
 
-@blueprint.route('/<template>')
-@login_required
-def route_template(template):
-    try:
-
-        if not template.endswith('.html'):
-            template += '.html'
-
-        # Detect the current page
-        segment = get_segment(request)
-
-        # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
-
-    except TemplateNotFound:
-        return render_template('home/page-404.html'), 404
-
-    except:
-        return render_template('home/page-500.html'), 500
+# @blueprint.route('/<template>')
+# @login_required
+# def route_template(template):
+#     try:
+#
+#         if not template.endswith('.html'):
+#             template += '.html'
+#
+#         # Detect the current page
+#         segment = get_segment(request)
+#
+#         # Serve the file (if exists) from app/templates/home/FILE.html
+#         return render_template("home/" + template, segment=segment)
+#
+#     except TemplateNotFound:
+#         return render_template('home/page-404.html'), 404
+#
+#     except:
+#         return render_template('home/page-500.html'), 500
 
 
 # Helper - Extract current page name from request
--- apps/pictures/routes.py
+++ apps/pictures/routes.py
@@ -3,9 +3,10 @@
 Copyright (c) 2019 - present AppSeed.us
 """
 import json
+import os
 
 from apps.pictures import blueprint
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_login import login_required
 
 from apps import db
@@ -23,7 +24,7 @@ def index():
 
 
 @blueprint.route('/data/<int:id>')
-# @login_required
+@login_required
 def data(id):
     data = Pictures.query.get_or_404(id)
     geo_codes = json.loads(data.geo_codes, parse_int=True)
@@ -33,11 +34,35 @@ def data(id):
 
 
 @blueprint.route('/all_data')
-# @login_required
+@login_required
 def all_data():
     pictures = Pictures.query.order_by(Pictures.id)
 
     return render_template('home/tables-data.html', pictures=pictures)
+
+
+@blueprint.route('/delete_data/<int:id>', methods=['POST'])
+@login_required
+def delete_data(id):
+    picture_data = Pictures.query.get_or_404(id)
+
+    if picture_data is None:
+        return "Error: No such data"
+
+    # remove the associated file
+    file_path = os.path.join(os.path.dirname(app.root_path), os.path.normpath(picture_data.path.lstrip("/")))
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print('file found', file_path)
+    else:
+        print(file_path)
+
+    # remove the data from database
+    db.session.delete(picture_data)
+    db.session.commit()
+
+    return redirect(url_for('pictures_blueprint.all_data'))
+
 
 
 
